7dc40467b7fc692c278257d5db5d2358daf7ccb8
cwadven <cwadven@naver.com>
2020-09-28 21:46:49 +0900

    게시글 작성자의 정보를 serializer로 가져오기

--- board/serializers.py
+++ board/serializers.py
@@ -1,8 +1,14 @@
 from .models import *
+from accounts.models import Profile
 from rest_framework import serializers
 
+class UserSerializer(serializers.ModelSerializer): #author 안에 있는 정보를 가져오기 위해서 TBoard랑 author로 관계가 있는 UserSerializer를 통해서 새로 만듦  
+    class Meta:
+        model = Profile
+        fields = ("id", "username", "nickname",)  
+
 class BoardSerializer(serializers.ModelSerializer):
-    author_username = serializers.ReadOnlyField(source='author.username')
+    author = UserSerializer(read_only=True)
     class Meta:
         model = Board
-        fields = ('id', 'author_username', 'title', 'body', 'created_at', 'updated_at')
+        fields = ('id', 'author', 'title', 'body', 'created_at', 'updated_at')
