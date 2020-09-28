cb2248ec101d5e0079488787e48b67e6fe4296eb
cwadven <cwadven@naver.com>
2020-09-28 19:06:33 +0900

    board 앱과 project urls.py 연결

---
+++ board/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.conf.urls import url
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter # class 를 경로 지정하기 위해서
+
+from . import views
+
+router = DefaultRouter() #view에 있는 함수(클래스를 가져오기 위해서는 router 사용)
+router.register('Board', views.BoardViewset)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
