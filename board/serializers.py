962087dbefcc6094c2e8b3b12898ab4827a2e578
cwadven <cwadven@naver.com>
2020-09-28 19:39:41 +0900

    사용자 이름 아이디로 보이게 설정

--- board/serializers.py
+++ board/serializers.py
@@ -2,6 +2,7 @@ from .models import *
 from rest_framework import serializers
 
 class BoardSerializer(serializers.ModelSerializer):
+    author_username = serializers.ReadOnlyField(source='author.username')
     class Meta:
         model = Board
-        fields = ('__all__')
+        fields = ('id', 'author_username', 'title', 'body', 'created_at', 'updated_at')
