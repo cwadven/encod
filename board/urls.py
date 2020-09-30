8ac877417b9017a42cbb0acc926231d8c57c4400
이창우 <cwadven@naver.com>
2020-10-01 00:00:41 +0900

    투표하는 기능들을 urls로 연결해주기

--- board/urls.py
+++ board/urls.py
@@ -8,7 +8,10 @@ from . import views
 
 router = DefaultRouter(trailing_slash=False) #view에 있는 함수(클래스를 가져오기 위해서는 router 사용)
 router.register('board', views.BoardViewset)
+# router.register('voteboard', views.VoteBoardView)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('voteboard', views.VoteBoardView.as_view(), name='vote_board'),
+    path('voteboard/<int:item_id>', views.VoteBoardDetailView.as_view(), name='vote_detail_board'),
 ]
