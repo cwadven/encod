5a210d012b626545f075edcd732cc5ff1536c815
이창우 <cwadven@naver.com>
2020-09-30 23:53:08 +0900

    투표하기 기능을 위해 모델을 구현

--- board/models.py
+++ board/models.py
@@ -12,7 +12,20 @@ class TimeStampedModel(models.Model):
 class Board(TimeStampedModel):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-    body = models.TextField()
+    voter = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='voter_total', blank=True)
+    ended = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
+
+class VoteBoard(TimeStampedModel):
+    boardid = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='contents')
+    title = models.CharField(max_length=100)
+    voter = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='voter_user', blank=True)
+    image = models.ImageField(blank=True, null=True, upload_to='uploads/')
+
+    def __str__(self):
+        return self.title
+
+    def voter_count(self):
+        return self.voter.count()
